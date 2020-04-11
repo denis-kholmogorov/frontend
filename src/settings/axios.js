9c5c3b52537e7ad02f46315942d7f00c3f19da1c
Igor Golovin <lislon@mail.ru>
2020-04-11 17:58:55 +0300

    Fix phonenumber display on profile page

--- src/settings/axios.js
+++ src/settings/axios.js
@@ -1,10 +1,14 @@
 import axios from 'axios'
 import store from '@/store'
 
+const NODE_ENV = process.env.NODE_ENV ? process.env.NODE_ENV : 'development';
+const IS_DEV = NODE_ENV === 'development';
 
 axios.defaults.headers['content-type'] = 'application/json';
 axios.defaults.withCredentials = true;
-axios.defaults.baseURL = 'https://virtserver.swaggerhub.com/andrewleykin/social/1.0.4/api/v1/'
+axios.defaults.baseURL =  IS_DEV
+  ? 'http://localhost:8086/api/v1/'
+  : 'https://virtserver.swaggerhub.com/andrewleykin/social/1.0.4/api/v1/';
 
 const token = localStorage.getItem('user-token')
 if (token) axios.defaults.headers.common['Authorization'] = token
