5efa690eabad8f6cbc725f7f147eeb2b20239f62
Igor Avdeev <lislon@mail.ru>
2020-04-12 13:36:56 +0300

    Prod build

--- src/settings/axios.js
+++ src/settings/axios.js
@@ -2,13 +2,19 @@ import axios from 'axios'
 import store from '@/store'
 
 const NODE_ENV = process.env.NODE_ENV ? process.env.NODE_ENV : 'development';
-const IS_DEV = NODE_ENV === 'development';
 
 axios.defaults.headers['content-type'] = 'application/json';
 axios.defaults.withCredentials = true;
-axios.defaults.baseURL =  IS_DEV
-  ? 'http://localhost:8086/api/v1/'
-  : 'https://virtserver.swaggerhub.com/andrewleykin/social/1.0.4/api/v1/';
+switch (NODE_ENV) {
+  case 'development': 
+    axios.defaults.baseURL =  'http://localhost:8086/api/v1/';
+    break;
+  case 'production': 
+    axios.defaults.baseURL =  '/api/v1/';
+    break;
+  default:
+    axios.defaults.baseURL =  'https://virtserver.swaggerhub.com/andrewleykin/social/1.0.4/api/v1/';
+}
 
 const token = localStorage.getItem('user-token')
 if (token) axios.defaults.headers.common['Authorization'] = token
