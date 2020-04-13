f0d4656d0705a0a7081f0b16225c73c7b2d7faf8
Igor Golovin <lislon@mail.ru>
2020-04-13 12:57:55 +0300

    Welcome page after registration

--- src/pages/Auth/Registration.vue
+++ src/pages/Auth/Registration.vue
@@ -64,7 +64,7 @@ export default {
       }
       const { email, passwd1, passwd2, firstName, lastName } = this
       this.register({ email, passwd1, passwd2, firstName, lastName, code: this.number }).then(() => {
-        this.$router.push({ name: 'News' })
+        this.$router.push({ name: 'RegisterSuccess' })
       })
     }
   },
